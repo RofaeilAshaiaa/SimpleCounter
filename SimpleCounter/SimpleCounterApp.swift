40b2811ecabea4276a8c2aba7daf2ad7f4dcd950
Rofaeil Ashaiaa <RofaeilAshaiaa@ProtonMail.com>
2021-05-15 01:25:29 +0200

    Initial Commit

---
+++ SimpleCounter/SimpleCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCounterApp.swift
+//  SimpleCounter
+//
+//  Created by matrix on 15/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
